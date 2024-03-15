53b85fd819bf5362b0bb91212327d1bda4e6e4b2
Bradley-Hill <brad.michael.hill.com>
2024-03-15 21:46:10 +0100

    Add size parameter to createGameboard function and update test case

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -1,4 +1,6 @@
-function createGameboard() {
+function createGameboard(size:number) {
+  //TODO: gameboard should be an array of arrays coordinates based on size parameter
+  const gameboard:[][] =  
   return {};
 }
 
--- tests/gameboard.test.ts
+++ tests/gameboard.test.ts
@@ -3,3 +3,11 @@ const gameboard = require("../src/gameboard");
 test("Does createGameboard return an object", () => {
   expect(typeof gameboard()).toBe("object");
 });
+
+test("The gameboard should be a 2d array of co-ordinates", () => {
+  const result = gameboard();
+  expect(Array.isArray(result)).toBe(true);
+  result.forEach((subArray: [][]) => {
+    expect(Array.isArray(subArray)).toBe(true);
+  });
+});
