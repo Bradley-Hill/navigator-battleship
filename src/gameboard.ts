55b0367edc0b47d0e377bab2fd245bd75695a21b
Bradley Hill <brad.michael.hill@gmail.com>
2024-03-16 18:30:56 +0000

    Refactor createGameboard function to use Cell objects in a 2D array

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -1,7 +1,16 @@
-function createGameboard(size:number) {
-  //TODO: gameboard should be an array of arrays coordinates based on size parameter
-  const gameboard:[][] =  
-  return {};
+type Cell = {
+  occupied: boolean;
+  hit: boolean;
+};
+
+function createGameboard(size: number): Cell[][] {
+  let gameboard = new Array(size);
+  gameboard = gameboard.map(() => new Array(size));
+  //using nested .map methods to add objects(Cell) to the gameboard(2d array)
+  gameboard = gameboard.map((row) =>
+    row.map(() => ({ occupied: false, hit: false }))
+  );
+  return gameboard;
 }
 
 module.exports = createGameboard;
