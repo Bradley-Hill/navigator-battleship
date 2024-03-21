9eefac5fc1c2c66a56a05811d900bb804800d441
Bradley-Hill <brad.michael.hill.com>
2024-03-21 14:25:03 +0100

    Refactor createPlayer function to accept isHuman parameter and assign name based on that.

--- src/player.ts
+++ src/player.ts
@@ -3,11 +3,13 @@ import { createGameboard, Gameboard } from "../src/gameboard";
 interface Player {
   isHuman: boolean;
   gameboard: Gameboard;
+  name: "Player 1" | "Player 2";
 }
 
-export function createPlayer(): Player {
+export function createPlayer(isHuman: boolean): Player {
   return {
-    isHuman: true,
+    isHuman: isHuman,
     gameboard: createGameboard(10),
+    name: isHuman ? "Player 1" : "Player 2",
   };
 }
--- tests/player.test.ts
+++ tests/player.test.ts
@@ -1,22 +1,22 @@
 import { createPlayer } from "../src/player";
 
 test("Expect the createPlayer function to return an object", () => {
-  const player = createPlayer();
+  const player = createPlayer(true);
   expect(typeof player).toBe("object");
 });
 
 test("Expect the player object to have a property isHuman", () => {
-  const player = createPlayer();
+  const player = createPlayer(true);
   expect(player).toHaveProperty("isHuman", true);
 });
 
 test("Expect the player object to have an instance of the gameboard associated", () => {
-  const player = createPlayer();
+  const player = createPlayer(false);
   expect(player).toHaveProperty("gameboard");
   expect(typeof player.gameboard.receiveAttack).toBe("function");
 });
 
 test("Expect the player object to have a property of name", () => {
-  const player = createPlayer();
+  const player = createPlayer(false);
   expect(player).toHaveProperty("name");
 });
