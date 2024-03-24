ad713007c9ec821c24c5f2374a0fa9d678f4cc9d
Bradley Hill <brad.michael.hill@gmail.com>
2024-03-24 13:47:53 +0000

    Add test for placing ships on gameboards in gameLoop

--- tests/gameLoop.test.ts
+++ tests/gameLoop.test.ts
@@ -9,3 +9,16 @@ test("Expect the gameLoop to create a human player and computer player,with game
   expect(gameLoop.humanPlayer.gameboard).toBeDefined();
   expect(gameLoop.compPlayer.gameboard).toBeDefined();
 });
+
+test("Expect the gameLoop to place 3 ships on each gameboard", () => {
+  const gameLoop = createGameLoop();
+  gameLoop.startGame();
+
+  expect(gameLoop.humanPlayer.gameboard.grid[0][0].ship).not.toBeNull();
+  expect(gameLoop.humanPlayer.gameboard.grid[0][1].ship).not.toBeNull();
+  expect(gameLoop.humanPlayer.gameboard.grid[0][2].ship).not.toBeNull();
+
+  expect(gameLoop.compPlayer.gameboard.grid[0][0].ship).not.toBeNull();
+  expect(gameLoop.compPlayer.gameboard.grid[0][1].ship).not.toBeNull();
+  expect(gameLoop.compPlayer.gameboard.grid[0][2].ship).not.toBeNull();
+});
