b9c7d4c3c7202b447209703c66a79dce53c8b172
Bradley-Hill <brad.michael.hill.com>
2024-03-27 14:30:47 +0100

    Add game setup and board layout,included DOM library in tsconfig

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,16 @@
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
-}
+import { createGameLoop } from "../src/gameLoop";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const startGameBtn = document.querySelector("#startBtn");
+  const opponentBoard = document.querySelector(".opponentBoard");
+  const playersBoard = document.querySelector(".playersBoard");
+
+  if (startGameBtn) {
+    startGameBtn.addEventListener("click", () => {
+      const gameLoop = createGameLoop();
+      gameLoop.startGame();
+    });
+  } else {
+    console.error("Start Game button not found");
+  }
+});
