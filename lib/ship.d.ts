fd9c00ea11586892ed9a4fb52c0133370b4e5abf
Bradley-Hill <brad.michael.hill.com>
2024-03-28 11:24:58 +0100

    Update script source file and remove unused files, using Parcel to bundle the files

--- lib/ship.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Ship {
-    length: number;
-    impacts: number;
-    sunk: boolean;
-    position: [number, number][] | null;
-    hit(): void;
-    isSunk(): void;
-}
-export declare function createShip(size: number): Ship;
