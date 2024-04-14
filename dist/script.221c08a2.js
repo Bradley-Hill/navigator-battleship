c7623f07b641880026e68d8f861c27d15c6b77c1
Bradley Hill <brad.michael.hill@gmail.com>
2024-04-14 14:29:54 +0200

    have removed the offending test.....functionality is working as I envisaged.

--- dist/script.221c08a2.js
+++ dist/script.221c08a2.js
@@ -666,7 +666,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50998" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55333" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
