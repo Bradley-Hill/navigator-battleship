5c16cfb407b905b16d53e444b83341e4536672ab
Bradley Hill <brad.michael.hill@gmail.com>
2024-04-14 14:30:35 +0200

    removed redundant commented out code

--- src/player.ts
+++ src/player.ts
@@ -23,56 +23,6 @@ export function createPlayer(isHuman: boolean): Player {
       opponent.gameboard.receiveAttack(x, y);
     },
 
-    // makeComputerMove: function (opponent: Player) {
-    //   if (!this.isHuman) {
-    //     if (opponent.gameboard.getHitCells().length > 0) {
-    //       let adjacentMoveModifiers: number[][] = [
-    //         [0, 1],
-    //         [1, 0],
-    //         [-1, 0],
-    //         [0, -1],
-    //       ];
-    //       let lastHitCell =
-    //         opponent.gameboard.getHitCells()[
-    //           opponent.gameboard.getHitCells().length - 1
-    //         ];
-    //       for (let i = 0; i < adjacentMoveModifiers.length; i++) {
-    //         let x = lastHitCell[0] + adjacentMoveModifiers[i][0];
-    //         let y = lastHitCell[1] + adjacentMoveModifiers[i][1];
-    //         if (
-    //           x >= 0 &&
-    //           x < opponent.gameboard.size &&
-    //           y >= 0 &&
-    //           y < opponent.gameboard.size &&
-    //           !opponent.gameboard
-    //             .getMissedShots()
-    //             .some((shot) => shot[0] === x && shot[1] === y)
-    //         ) {
-    //           opponent.gameboard.receiveAttack(x, y);
-    //           return;
-    //         }
-    //       }
-    //     }
-    //     let validRandomMove = false;
-    //     let x: number = 0;
-    //     let y: number = 0;
-    //     while (!validRandomMove) {
-    //       x = Math.floor(Math.random() * opponent.gameboard.size);
-    //       y = Math.floor(Math.random() * opponent.gameboard.size);
-
-    //       const missedShots = opponent.gameboard.getMissedShots();
-    //       const hitCells = opponent.gameboard.getHitCells();
-    //       if (
-    //         !missedShots.some((shot) => shot[0] === x && shot[1] == y) &&
-    //         !hitCells.some((cell) => cell[0] === x && cell[1] === y)
-    //       ) {
-    //         validRandomMove = true;
-    //       }
-    //     }
-    //     opponent.gameboard.receiveAttack(x, y);
-    //   }
-    //   console.log("Computer made a move");
-    // },
     toggleTurn: function () {
       this.isMyTurn = !this.isMyTurn;
     },
