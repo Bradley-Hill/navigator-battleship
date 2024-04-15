fec533fdef7bcd77814e63bd3165722f5b05de69
Bradley-Hill <brad.michael.hill.com>
2024-04-15 09:34:30 +0200

    Refactor createPlayer function to include isHardDifficulty parameter

--- tests/player.test.ts
+++ tests/player.test.ts
@@ -1,40 +1,40 @@
 import { createPlayer } from "../src/player";
 
 test("Expect the createPlayer function to return an object", () => {
-  const player = createPlayer(true);
+  const player = createPlayer(true, false);
   expect(typeof player).toBe("object");
 });
 
 test("Expect the player object to have a property isHuman", () => {
-  const player = createPlayer(true);
+  const player = createPlayer(true, false);
   expect(player).toHaveProperty("isHuman", true);
 });
 
 test("Expect the player object to have an instance of the gameboard associated", () => {
-  const player = createPlayer(false);
+  const player = createPlayer(false, false);
   expect(player).toHaveProperty("gameboard");
   expect(typeof player.gameboard.receiveAttack).toBe("function");
 });
 
 test("Expect the player object to have a property of name", () => {
-  const player = createPlayer(false);
+  const player = createPlayer(false, false);
   expect(player).toHaveProperty("name");
 });
 
 test("Expect the player object to have a isMyTurn property", () => {
-  const player = createPlayer(true);
+  const player = createPlayer(true, false);
   expect(player).toHaveProperty("isMyTurn");
 });
 
 test("Expect the player object to have a method to make an attack on the other players gameboard", () => {
-  const player = createPlayer(true);
+  const player = createPlayer(true, false);
   expect(player).toHaveProperty("makeHumanMove");
   expect(typeof player.makeHumanMove).toBe("function");
 });
 
 test("Expect the makeMove method to correctly submit an attack to the other players gameboard", () => {
-  const humanPlayer = createPlayer(true);
-  const compPlayer = createPlayer(false);
+  const humanPlayer = createPlayer(true, false);
+  const compPlayer = createPlayer(false, false);
   compPlayer.gameboard.createShips(1, 1, 2, "vertical");
   humanPlayer.makeHumanMove(1, 1, compPlayer);
   humanPlayer.makeHumanMove(1, 2, compPlayer);
@@ -42,8 +42,8 @@ test("Expect the makeMove method to correctly submit an attack to the other play
 });
 
 test("Expect the opponents gameboard to report the missed shots and hit coordinates", () => {
-  const humanPlayer = createPlayer(true);
-  const compPlayer = createPlayer(false);
+  const humanPlayer = createPlayer(true, false);
+  const compPlayer = createPlayer(false, false);
   compPlayer.gameboard.createShips(1, 1, 3, "vertical");
   humanPlayer.makeHumanMove(1, 3, compPlayer);
   humanPlayer.makeHumanMove(1, 6, compPlayer);
@@ -56,8 +56,8 @@ test("Expect the opponents gameboard to report the missed shots and hit coordina
 });
 
 test("Expect computer player to make a valid random move", () => {
-  const humanPlayer = createPlayer(true);
-  const compPlayer = createPlayer(false);
+  const humanPlayer = createPlayer(true, false);
+  const compPlayer = createPlayer(false, false);
   humanPlayer.gameboard.createShips(1, 1, 3, "vertical");
 
   compPlayer.makeComputerMove(humanPlayer);
@@ -68,8 +68,8 @@ test("Expect computer player to make a valid random move", () => {
 });
 
 test("Expect the computer player to make a move adjacent to a confirmed hit", () => {
-  const humanPlayer = createPlayer(true);
-  const compPlayer = createPlayer(false);
+  const humanPlayer = createPlayer(true, false);
+  const compPlayer = createPlayer(false, false);
   humanPlayer.gameboard.createShips(1, 1, 3, "vertical");
 
   let hit = false;
